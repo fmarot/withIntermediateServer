67e0aad1ed1d27b8f019c99b50784e70fe527a01
Francois Marot <Francois.Marot@olea-medical.com>
2013-10-03 18:01:33 +0200

    WIP

--- intermediateserver/src/main/java/AppSpringConfig.java
+++ intermediateserver/src/main/java/AppSpringConfig.java
@@ -3,6 +3,7 @@ import java.util.Map;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.remoting.httpinvoker.HttpInvokerProxyFactoryBean;
 import org.springframework.remoting.httpinvoker.SimpleHttpInvokerServiceExporter;
 import org.springframework.remoting.support.SimpleHttpServerFactoryBean;
 
@@ -12,26 +13,37 @@ import com.sun.net.httpserver.HttpHandler;
 @Configuration
 public class AppSpringConfig {
 
+	/* *********** Local service exposed in HTTPInvoker ************ */
 	@Bean
-	public PresetService presetService() {
+	public PresetService localPresetService() {
 		return new PresetServiceImpl();
 	}
 
 	@Bean
 	public SimpleHttpInvokerServiceExporter serviceExporter() {
 		SimpleHttpInvokerServiceExporter exporter = new SimpleHttpInvokerServiceExporter();
-		exporter.setService(this.presetService());
+		exporter.setService(this.localPresetService());
 		exporter.setServiceInterface(PresetService.class);
 		return exporter;
 	}
 
 	@Bean
-	public SimpleHttpServerFactoryBean remotePresetService() {
+	public SimpleHttpServerFactoryBean presetService() {
 		Map<String, HttpHandler> contexts = new HashMap<String, HttpHandler>();
 		contexts.put("/remoting/PresetService", serviceExporter());
 		SimpleHttpServerFactoryBean serverFactory = new SimpleHttpServerFactoryBean();
 		serverFactory.setContexts(contexts);
 		serverFactory.setPort(8080);
 		return serverFactory;
+	}
+
+	/* ************* Remote service accessed ************** */
+	@Bean
+	public HttpInvokerProxyFactoryBean remotePresetService() {
+		HttpInvokerProxyFactoryBean service = new HttpInvokerProxyFactoryBean();
+		service.setServiceInterface(PresetService.class);
+		String url = "http://127.0.0.1:8090/remoting/PresetService";
+		service.setServiceUrl(url);
+		return service;
 	}
 }
